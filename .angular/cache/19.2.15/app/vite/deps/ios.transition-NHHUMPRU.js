e5406d089fbd7e91473950ee6322ee84b59ffa7c
rosimerebr <rosipdf@gmail.com>
2025-06-12 17:32:09 +0100

    Creat app

---
+++ .angular/cache/19.2.15/app/vite/deps/ios.transition-NHHUMPRU.js
@@ -0,0 +1,14 @@
+import {
+  iosTransitionAnimation,
+  shadow
+} from "./chunk-WB54BB6M.js";
+import "./chunk-VYA3E5QR.js";
+import "./chunk-44IZU6OF.js";
+import "./chunk-EIYVLOJ2.js";
+import "./chunk-R4SJUBFW.js";
+import "./chunk-CBIR4FRL.js";
+import "./chunk-QHQP2P2Z.js";
+export {
+  iosTransitionAnimation,
+  shadow
+};
---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
+  },
+];
