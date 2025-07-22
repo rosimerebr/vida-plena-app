473f31584a16be4fd6a070f742b916db206e5c6d
rosimerebr <rosipdf@gmail.com>
2025-07-22 10:27:23 +0100

    feat: add logout button to side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { IonApp, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet } from '@ionic/angular/standalone';
 import { RouterModule } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { addIcons } from 'ionicons';
+import { home, personCircle, leaf, walk, barChart, informationCircle, logOut } from 'ionicons/icons';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +15,16 @@ import { RouterModule } from '@angular/router';
     RouterModule
   ]
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {
+    addIcons({ home, personCircle, leaf, walk, barChart, informationCircle, logOut });
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
